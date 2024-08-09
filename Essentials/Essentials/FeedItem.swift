7749fa0ad96e444a08698d83540fb850be9b44b7
vishnu-emani <emanivishnuvasu@gmail.com>
2024-08-09 18:19:18 +0530

    Create feed model

---
+++ Essentials/Essentials/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  Essentials
+//
+//  Created by Vishnu on 09/08/24.
+//
+
+import Foundation
+
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
