80fbaae37db14d916c4e0082ce20ee3016aade29
vishnu-emani <emanivishnuvasu@gmail.com>
2024-08-10 19:25:03 +0530

    essentials/RequestDataLoader

--- Essentials/EssentialsTests/RemoteFeedLoaderTests.swift
+++ Essentials/EssentialsTests/RemoteFeedLoaderTests.swift
@@ -10,20 +10,37 @@ import Foundation
 import XCTest
 
 class RemoteFeedLoader {
-    
+  
+    func load() {
+        
+        HttpClient.sharedInstance.requestedURL = URL(string: "https://a-url")
+    }
 }
+
 class HttpClient {
+    static let sharedInstance = HttpClient()
     
+    private init() {}
     var requestedURL: URL?
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client =  HttpClient()
+        let client =  HttpClient.sharedInstance
         let sut = RemoteFeedLoader()
     
         XCTAssertNil(client.requestedURL)
         
     }
+    
+    func test_load_requestDataFromURL() {
+        let client =  HttpClient.sharedInstance
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+    }
+    
 }
