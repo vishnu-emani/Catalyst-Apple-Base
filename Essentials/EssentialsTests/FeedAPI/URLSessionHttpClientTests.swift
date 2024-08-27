ae1a2a26e8a758f6c4a7b54855e8df97ebe9510b
vishnu-emani <emanivishnuvasu@gmail.com>
2024-08-27 15:47:25 +0530

    move the urlsessionHTTPCLient sut method to factory method

--- Essentials/EssentialsTests/FeedAPI/URLSessionHttpClientTests.swift
+++ Essentials/EssentialsTests/FeedAPI/URLSessionHttpClientTests.swift
@@ -44,8 +44,8 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         
-        URLSessionHTTPClient().get(from: url) {_ in }
-        wait(for: [exp], timeout: 1.0)
+        makeSUT().get(from: url) {_ in }
+        wait(for: HTTPClient()[exp], timeout: 1.0)
     }
     
     func test_getFromURL_failsOnRequestError() {
@@ -53,11 +53,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         
-        let sut = URLSessionHTTPClient()
-        
         let exp = expectation(description: "Wait for completion")
         
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -73,6 +71,10 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
 
     // MARK: - Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        URLSessionHTTPClient()
+    }
     
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
