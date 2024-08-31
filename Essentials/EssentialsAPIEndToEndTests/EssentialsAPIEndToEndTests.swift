0b31c9ed4cc99b078b6eee63e3a0a3601c3b516a
vishnu-emani <emanivishnuvasu@gmail.com>
2024-08-31 19:15:07 +0530

    extract loader to helper method

--- Essentials/EssentialsAPIEndToEndTests/EssentialsAPIEndToEndTests.swift
+++ Essentials/EssentialsAPIEndToEndTests/EssentialsAPIEndToEndTests.swift
@@ -6,30 +6,103 @@
 //
 
 import XCTest
+import Essentials
 
 final class EssentialsAPIEndToEndTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_endToEndTestServerGetFeedResult_matchesFixedTestAccountData() {
+        switch getFeedResult() {
+        case let .success(items)? :
+            XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
+            XCTAssertEqual(items[0], expectedItem(at: 0))
+            XCTAssertEqual(items[1], expectedItem(at: 1))
+            XCTAssertEqual(items[2], expectedItem(at: 2))
+            XCTAssertEqual(items[3], expectedItem(at: 3))
+            XCTAssertEqual(items[4], expectedItem(at: 4))
+            XCTAssertEqual(items[5], expectedItem(at: 5))
+            XCTAssertEqual(items[6], expectedItem(at: 6))
+            XCTAssertEqual(items[7], expectedItem(at: 7))
+ 
+            // can use enumeration if we have more items
+            //            items.enumerated().forEach { (index, item) in
+            //                XCTAssertEqual(item, expectedItem(at: index))
+            //            }
+            
+        case let .failure(error)? :
+            XCTFail("Expected successful feed result, got \(error) instead")
+        default:
+            XCTFail("Expected successful feed result, got no result instead")
         }
     }
+    
+    // MARK: - Helpers
+    
+    private func getFeedResult() -> LoadFeedResult? {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        var receivedResult: LoadFeedResult?
+        
+        let exp = expectation(description: "Wait for load completion")
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 5.0)
+        return receivedResult
+    }
+
+        private func expectedItem(at index: Int) -> FeedItem {
+            return FeedItem(
+                id: id(at: index),
+                description: description(at: index),
+                location: location(at: index),
+                imageURL: imageURL(at: index))
+        }
+
+        private func id(at index: Int) -> UUID {
+            return UUID(uuidString: [
+                "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+                "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+                "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+                "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+                "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+                "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+                "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+                "F79BD7F8-063F-46E2-8147-A67635C3BB01"
+            ][index])!
+        }
+
+        private func description(at index: Int) -> String? {
+            return [
+                "Description 1",
+                nil,
+                "Description 3",
+                nil,
+                "Description 5",
+                "Description 6",
+                "Description 7",
+                "Description 8"
+            ][index]
+        }
+
+        private func location(at index: Int) -> String? {
+            return [
+                "Location 1",
+                "Location 2",
+                nil,
+                nil,
+                "Location 5",
+                "Location 6",
+                "Location 7",
+                "Location 8"
+            ][index]
+        }
+
+        private func imageURL(at index: Int) -> URL {
+            return URL(string: "https://url-\(index+1).com")!
+        }
 
 }
