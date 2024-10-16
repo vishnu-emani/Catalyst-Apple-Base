3e91654b02767f13b8a184f740cc10db08824f58
vishnu-emani <emanivishnuvasu@gmail.com>
2024-10-16 13:39:30 +0530

    updated readme and changed the remote feed loader tests name

--- Essentials/EssentialsTests/FeedAPI/LoadFeedFromRemoteUseCaseTests.swift
+++ Essentials/EssentialsTests/FeedAPI/LoadFeedFromRemoteUseCaseTests.swift
@@ -10,7 +10,7 @@ import Foundation
 import XCTest
 import Essentials
 
-class RemoteFeedLoaderTests: XCTestCase {
+class LoadFeedFromRemoteUseCaseTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         
